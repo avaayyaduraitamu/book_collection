93bd0a91e51a293ec59d6ecb0e085c361b655173
Avanthika_Ayyadurai <avaayyaduraitamu@tamu.edu>
2026-09-14 23:27:38 -0500

    Updated views and changed the routes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,9 @@
 class Book < ApplicationRecord
+  has_many :user_books
+  has_many :users, through: :user_books
+
   validates :title, presence: true
-   validates :author, presence: true
+  validates :author, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
-   validates :published_date, presence: true
+  validates :published_date, presence: true
 end
